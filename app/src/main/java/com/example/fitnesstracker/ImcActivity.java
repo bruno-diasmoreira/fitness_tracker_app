1d185422b109f40cb8aa7884dd16faf7ae1fd556
brunodiasmoreira <brunodiasmoreira2016@gmail.com>
2021-09-26 12:40:05 -0300

    Implementando as funções para fazer o calculo imc

--- app/src/main/java/com/example/fitnesstracker/ImcActivity.java
+++ app/src/main/java/com/example/fitnesstracker/ImcActivity.java
@@ -29,7 +29,60 @@ public class ImcActivity extends AppCompatActivity {
         btnCalc = findViewById(R.id.btn_calc);
 
 
+        btnCalc.setOnClickListener(view -> {
+
+            if (!validate()) {
+                Toast.makeText(getApplicationContext(), "Preencha os campos corretamente", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            int peso = Integer.parseInt(editWeight.getText().toString());
+            int altura = Integer.parseInt(editHeight.getText().toString());
+
+
+            double calc = calculate(peso, altura);
+
+            int response = imcResponse(calc);
+
+            AlertDialog alertDialog = new AlertDialog.Builder(ImcActivity.this).setTitle(getString(R.string.imc_response, calc))
+                    .setMessage(response)
+                    .setPositiveButton(android.R.string.ok, (dialogInterface, i) -> {
+
+                    })
+                    .create();
+
+            alertDialog.show();
+
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(editHeight.getWindowToken(), 0);
+            imm.hideSoftInputFromWindow(editWeight.getWindowToken(), 0);
+
+        });
     }
 
+    private double calculate(int peso, int altura) {
+        return peso / (((double) altura / 100) * ((double) altura / 100));
+    }
 
+    @StringRes
+    private int imcResponse(double imc) {
+        if (imc < 18.5) {
+            return R.string.magreza;
+        } else if (imc >= 18.5 && imc <= 24.9) {
+            return R.string.pesoNormal;
+        } else if (imc >= 25 && imc <= 29.9) {
+            return R.string.sobrepeso;
+        } else if (imc >= 30 && imc <= 39.9) {
+            return R.string.obesidade;
+        } else {
+            return R.string.obesidadeGrave;
+        }
+    }
+
+    private boolean validate() {
+        return (!editWeight.getText().toString().isEmpty() &&
+                !editHeight.getText().toString().isEmpty() &&
+                !editWeight.getText().toString().startsWith("0") &&
+                !editHeight.getText().toString().startsWith("0"));
+    }
 }
